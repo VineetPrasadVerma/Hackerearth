1e3b510ef5ca18afd93a3c5163083604b829e3a4
Vineet Prasad Verma <vineetpd1996@gmail.com>
2019-06-08 18:28:17 +0530

    Solved Sitting Arrangement

---
+++ BasicProgramming/BasicOfInputAndOutput/SeatingArrangement.py
@@ -0,0 +1,33 @@
+n = int(input())
+for _ in range(n):
+    flag = 0
+    seat_number = int(input())
+    list_of_seat_type = ['WS', 'MS', 'AS', 'AS', 'MS', 'WS']
+    seat_type = ""
+    for i in range(6, 114, 6):
+        difference = 11
+        if flag == 0:
+            temp = 0
+            for j in range(i-6+1, i+1):
+                seat_type = list_of_seat_type[temp]
+                temp += 1
+                flag = 1
+                if seat_number in range(i-6+1, i+1):
+                    facing_seat_number = j + difference
+                    difference -= 2
+                    if seat_number == j:
+                        print(facing_seat_number, end=" ")
+                        print(seat_type)
+        else:
+            difference = 1
+            temp = 0
+            for j in range(i-6+1, i+1):
+                seat_type = list_of_seat_type[temp]
+                temp += 1
+                flag = 0
+                if seat_number in range(i-6+1, i+1):
+                    facing_seat_number = j - difference
+                    difference += 2
+                    if seat_number == j:
+                        print(facing_seat_number, end=" ")
+                        print(seat_type)
